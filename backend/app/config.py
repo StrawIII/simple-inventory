1580ed179eb9b814d379b18bfa51a6b0be52ff92
Roman Seiner <romisseiner@gmail.com>
2024-07-09 17:33:45 +0200

    Add postgres_dsn property

---
+++ backend/app/config.py
@@ -0,0 +1,41 @@
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    project_name: str = "Simple Inventory System"
+
+    jwt_alg: str = "HS256"
+    jwt_key: str = "secret"
+
+    # TODO move to .env
+    postgres_host: str = "localhost"
+    postgres_port: int = 5432
+    postgres_user: str = "postgres"
+    postgres_password: str = "postgres"
+    postgres_db: str = "simple_inventory"
+
+    @computed_field
+    @property
+    def postgres_dsn(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg2",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=self.postgres_db,
+        )
+
+    # MINIO_SERVER: str
+    # MINIO_PORT: int
+    # MINIO_USER: str
+    # MINIO_PASSWORD: str
+
+    # class Config:
+    #     env_file: str = find_dotenv()
+
+
+settings = Settings()
