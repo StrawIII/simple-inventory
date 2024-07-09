2e66e59f7653ae39982acf4d87cbb7c45d74cc33
Roman Seiner <romisseiner@gmail.com>
2024-07-09 17:34:05 +0200

    Create crud.py

---
+++ backend/app/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+
+from app.models import User
+
+
+def create_user(*, session: Session):
+    pass
+
+
+def update_user(*, session: Session):
+    pass
+
+
+def delete_user(*, session: Session):
+    pass
