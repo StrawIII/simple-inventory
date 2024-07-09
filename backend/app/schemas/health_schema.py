a7c8495d6a4bcea9af0372e20ea9ad742fcf07e6
Roman Seiner <romisseiner@gmail.com>
2024-07-09 17:30:21 +0200

    Refactor

--- backend/app/schemas/health_schema.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from typing import Literal
-
-from pydantic import BaseModel
-
-
-class Health(BaseModel):
-    status: Literal["healthy"]
-    timestamp: datetime
--- backend/app/schemas/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserCreds(BaseModel):
-    username: str
-    password: str
