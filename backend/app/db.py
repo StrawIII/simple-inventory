0247bde507b534c43f568dbd8561d24e122c8aa6
Roman Seiner <romisseiner@gmail.com>
2024-07-30 22:38:13 +0200

    Update type annotations

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,7 @@
-from typing import Annotated, Any
+from typing import Annotated
 
 import boto3
+from botocore.client import BaseClient
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -17,17 +18,26 @@ def get_db():
 
 DBDep = Annotated[Session, Depends(get_db)]
 
-
-s3 = boto3.client(
+s3: BaseClient = boto3.client(
     "s3",
-    endpoint_url="http://localhost:9000",
-    aws_access_key_id="minio_access_key",
-    aws_secret_access_key="minio_secret_key",
+    endpoint_url=f"{settings.minio_host}:{settings.minio_port}",
+    aws_access_key_id=settings.minio_access_key,
+    aws_secret_access_key=settings.minio_secret_key,
 )
 
 
-def get_s3_client():
+def get_s3_client() -> BaseClient:
     return s3
 
 
-S3Dep = Annotated[Any, Depends(get_s3_client)]
+S3Dep = Annotated[BaseClient, Depends(get_s3_client)]
+
+if __name__ == "__main__":
+    s3 = boto3.client(
+        "s3",
+        endpoint_url="http://localhost:9000",
+        aws_access_key_id="z0LcizhPp7MrM41Z9OE3",
+        aws_secret_access_key="PiDrP3ZaGT1pQWWJqvs7Khngo7gL4iRwYDiTQGyH",
+    )
+    for bucket in s3.list_buckets()["Buckets"]:
+        print(f'  {bucket["Name"]}')
