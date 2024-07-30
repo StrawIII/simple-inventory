0939e52393ae04be2b840f1aba6cd36895f5d3da
Roman Seiner <romisseiner@gmail.com>
2024-07-30 22:39:50 +0200

    Update users.py

--- backend/app/api/routers/users.py
+++ backend/app/api/routers/users.py
@@ -1,29 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy.exc import IntegrityError
+
+from app.db import DBDep
+from app.models import Item, User
+from app.schemas import UserInUI
+from app.security import CurrentUserDep, IsAdminDep, hash_password
 
 router = APIRouter(prefix="/users")
 
 
-# only used by admin
 @router.get("")
-def get_users():
-    return "OK"
+def get_users(db: DBDep, is_admin: IsAdminDep):
+    return db.query(User).all()
 
 
 @router.get("/{username}")
-def get_user():
+def get_user(username: str):
     return "OK"
 
 
-@router.post("/{username}")
-def create_user():
-    return "OK"
+@router.post("/users", status_code=201)
+def create_user_(user: UserInUI, db: DBDep):
+    db.add(
+        User(
+            **user.model_dump(exclude={"password"}),
+            hashed_password=hash_password(user.password),
+        )
+    )
+
+    try:
+        db.commit()
+    except IntegrityError as e:
+        db.rollback()
+        # TODO more granualar responses
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred while creating the user: {e}",
+        )
 
 
 @router.put("/{username}")
-def update_user():
+def update_user(username: str):
     return "OK"
 
 
 @router.get("/{username}/items")
-def get_items(username: str):
-    return "OK"
+def get_items(username: str, db: DBDep, current_user: CurrentUserDep):
+    return db.query(Item).filter(Item.owner_id == current_user)
