e3b74fb82f3dd19e83bc3a3c0018a0297eec362a
Roman Seiner <romisseiner@gmail.com>
2024-07-30 22:41:15 +0200

    Update database using alembic

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import ForeignKey, String, Text
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, Boolean, ForeignKey, Integer, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -8,18 +10,50 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "user"
-    # __table_args__ = {"schema": "simple_inventory"}  # docs recommend default "public" schema
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String, unique=True)
-    # admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(Text, unique=True)
+    email: Mapped[str] = mapped_column(Text, unique=True)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    hashed_password: Mapped[str] = mapped_column(Text)
 
 
 class Item(Base):
     __tablename__ = "item"
 
-    id: Mapped[str] = mapped_column(String(255), primary_key=True)
-    owner: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    comment: Mapped[str] = mapped_column(Text)
-    location: Mapped[str] = mapped_column(String(255))
-    location_comment: Mapped[str] = mapped_column(Text)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    external_id: Mapped[str] = mapped_column(Text, nullable=True, unique=True)
+    name: Mapped[str] = mapped_column(Text)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    status: Mapped[str] = mapped_column(
+        ForeignKey("item_status.name"), default="AVAILABLE"
+    )
+    comment: Mapped[str] = mapped_column(Text, nullable=True)
+    location: Mapped[str] = mapped_column(Text)
+    location_comment: Mapped[str] = mapped_column(Text, nullable=True)
+    image: Mapped[str] = mapped_column(Text, nullable=True, unique=True)
+
+
+class Borrow(Base):
+    __tablename__ = "borrow"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id"))
+    status: Mapped[str] = mapped_column(
+        ForeignKey("borrow_status.name"), default="PENDING"
+    )
+    timestamp_from: Mapped[datetime] = mapped_column(TIMESTAMP)
+    timestamp_to: Mapped[datetime] = mapped_column(TIMESTAMP)
+
+
+class ItemStatus(Base):
+    __tablename__ = "item_status"
+
+    name: Mapped[str] = mapped_column(Text, primary_key=True)
+
+
+class BorrowStatus(Base):
+    __tablename__ = "borrow_status"
+
+    name: Mapped[str] = mapped_column(Text, primary_key=True)
