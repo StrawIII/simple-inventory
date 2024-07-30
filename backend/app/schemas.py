f03cb9c26a3d1732006c769330fe6d58a700d76e
Roman Seiner <romisseiner@gmail.com>
2024-07-30 22:41:43 +0200

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from typing import Literal
+from typing import Literal, Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, Field
 
 
 # TODO implement "unhealthy" status (e.g., connectiong to database failed)
@@ -11,67 +11,80 @@ class Health(BaseModel):
 
 
 class UserCreds(BaseModel):
-    email: EmailStr
+    username: str
     password: str
 
 
-class UserBase(BaseModel):
-    email: EmailStr
+class UserInUI(BaseModel):
+    username: str
+    email: str
+    password: str
 
 
-class ItemBase(BaseModel):
-    pass
-
-
-class ItemFromFile(ItemBase):
-    item_type: str = Field(alias="Typ")
+class ItemFromFile(BaseModel):
+    # item_type: str = Field(alias="Typ")
     id: str = Field(alias="Inv. číslo")
     name: str = Field(alias="Název")
-    serial_number: str = Field("", alias="Výr. číslo")
-    inclusion_date: datetime = Field(alias="Dat. zařazení")
-    ns: int = Field(alias="NS")
-    location_id: str = Field(alias="Kód umístění")
-    location_name: str = Field(alias="Název umístění")
-    classification: str = Field(alias="Klasifikace")
-    initial_accounting_price: float = Field(alias="Vstupní účetní cena")
-    residual_price: float = Field(alias="Zůst. cena")
+    # serial_number: str = Field("", alias="Výr. číslo")
+    # inclusion_date: datetime = Field(alias="Dat. zařazení")
+    # ns: int = Field(alias="NS")
+    # location_id: str = Field(alias="Kód umístění")
+    location: str = Field(alias="Název umístění")
+    # classification: str = Field(alias="Klasifikace")
+    # initial_accounting_price: float = Field(alias="Vstupní účetní cena")
+    # residual_price: float = Field(alias="Zůst. cena")
     comment: str = Field(alias="Poznámka")
-    tangible_deduction: bool = Field(alias="Hmot. odp.")
+    # tangible_deduction: bool = Field(alias="Hmot. odp.")
 
-    @field_validator("inclusion_date", mode="before")
-    def format_date(cls, v: str):
-        try:
-            return datetime.strptime(v, r"%d.%m.%Y")
-        except ValueError:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Invalid date format: '{v}'. Expected date format: DD.MM.YYYY",
-            )
+    # @field_validator("inclusion_date", mode="before")
+    # def format_date(cls, v: str):
+    #     try:
+    #         return datetime.strptime(v, r"%d.%m.%Y")
+    #     except ValueError:
+    #         raise HTTPException(
+    #             status_code=status.HTTP_400_BAD_REQUEST,
+    #             detail=f"Invalid date format: '{v}'. Expected date format: DD.MM.YYYY",
+    #         )
 
-    @field_validator("initial_accounting_price", "residual_price", mode="before")
-    def format_price(cls, v: str):
-        try:
-            float(re.sub(r"\s+", "", v).replace(",", "."))
-        except ValueError:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Invalid number format: '{v}'. Expected number format: '1 234,56'",
-            )
+    # @field_validator("initial_accounting_price", "residual_price", mode="before")
+    # def format_price(cls, v: str):
+    #     try:
+    #         return float(re.sub(r"\s+", "", v).replace(",", "."))
+    #     except ValueError:
+    #         raise HTTPException(
+    #             status_code=status.HTTP_400_BAD_REQUEST,
+    #             detail=f"Invalid number format: '{v}'. Expected number format: '1 234,56'",
+    #         )
 
-    @field_validator("tangible_deduction", mode="before")
-    def tangible_deduction_validator(cls, v: str):
-        if v == "ANO":
-            return True
+    # @field_validator("tangible_deduction", mode="before")
+    # def tangible_deduction_validator(cls, v: str):
+    #     if v == "ANO":
+    #         return True
 
-        if v == "NE":
-            return False
+    #     if v == "NE":
+    #         return False
 
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Invalid value for 'Hmot. odp.': '{v}'. Expected 'ANO' or 'NE'.",
-        )
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail=f"Invalid value for 'Hmot. odp.': '{v}'. Expected 'ANO' or 'NE'.",
+    #     )
 
 
-class ItemFromWeb(ItemBase):
-    pass
+class ItemFromUI(BaseModel):
+    name: str
+    comment: Optional[str] = None
+    location: Optional[str] = None
+    location_comment: Optional[str] = None
 
+
+class ItemUpdate(BaseModel):
+    name: str
+    comment: Optional[str] = None
+    location: Optional[str] = None
+    location_comment: Optional[str] = None
+
+
+class BorrowRequest(BaseModel):
+    item_id: int
+    timestamp_from: datetime
+    timestamp_to: datetime
