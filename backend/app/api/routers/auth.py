44b62310105a28e1fe5df669ac12479aa624791a
Roman Seiner <romisseiner@gmail.com>
2024-07-30 22:40:17 +0200

    Update auth.py

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -1,20 +1,47 @@
-from typing import Union
+from fastapi import APIRouter, Request, Response
 
-from fastapi import APIRouter, Header
-from typing_extensions import Annotated
-
+from app.config import SettingsDep
+from app.db import DBDep
+from app.models import User
 from app.schemas import UserCreds
-from app.security import create_access_token, verify_access_token
+from app.security import create_access_token, verify_password
 
 router = APIRouter()
 
 
-# TODO remember me -> longer exp
-@router.post("/auth")
-def auth(creds: UserCreds, authorization: Annotated[Union[str, None], Header()] = None):
-    if authorization:
-        valid = verify_access_token(user=creds.username, token=authorization)
-        # return RedirectResponse(url="https://www.google.com")
-        return {"valid": valid, "user": creds.username}
+@router.post("/login")
+def login(response: Response, creds: UserCreds, settings: SettingsDep, db: DBDep):
+    user = db.query(User).filter_by(username=creds.username).first()
 
-    return {"token": create_access_token(user=creds.username)}
+    verify_password(creds.password, user.hashed_password)
+
+    response.set_cookie(
+        key=settings.cookie_key,
+        value=create_access_token(creds.username),
+        httponly=True,
+    )
+
+    return creds.username
+
+
+# * test route
+@router.get("/test/token/{username}")
+def token(username: str, response: Response, settings: SettingsDep):
+    token = create_access_token(username)
+
+    response.set_cookie(
+        key=settings.cookie_key,
+        value=token,
+        max_age=settings.max_age,
+        httponly=True,
+    )
+    return "OK"
+
+
+# * test route
+@router.get("/test/cookie")
+def get_cookie(request: Request, settings: SettingsDep):
+    cookie_value = request.cookies.get(settings.cookie_key)
+    # print(request.headers)
+    # print(request.cookies)
+    return cookie_value
