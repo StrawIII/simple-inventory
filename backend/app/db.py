9b8e630255bc2a33e294ce36ba38c566893c5cd6
Roman Seiner <romisseiner@gmail.com>
2024-08-13 23:02:48 +0200

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -21,8 +21,8 @@ DBDep = Annotated[Session, Depends(get_db)]
 s3: BaseClient = boto3.client(
     "s3",
     endpoint_url=f"{settings.minio_host}:{settings.minio_port}",
-    aws_access_key_id=settings.minio_access_key,
-    aws_secret_access_key=settings.minio_secret_key,
+    aws_access_key_id=settings.minio_root_user,
+    aws_secret_access_key=settings.minio_root_password,
 )
 
 
@@ -31,16 +31,6 @@ def get_s3_client() -> BaseClient:
 
 
 S3Dep = Annotated[BaseClient, Depends(get_s3_client)]
-
-if __name__ == "__main__":
-    s3 = boto3.client(
-        "s3",
-        endpoint_url="http://localhost:9000",
-        aws_access_key_id="z0LcizhPp7MrM41Z9OE3",
-        aws_secret_access_key="PiDrP3ZaGT1pQWWJqvs7Khngo7gL4iRwYDiTQGyH",
-    )
-    for bucket in s3.list_buckets()["Buckets"]:
-        print(f'  {bucket["Name"]}')
 
 
 # ? move to crud.py
